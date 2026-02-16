7aef4dcdb74f9a83941286e8c534455b0d9a7036
RavinduRepo <e20280@eng.pdn.ac.lk>
2026-02-16 09:36:02 +0530

    added cpp debugings with assemler view in production mode

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,1 @@
-return {
-  "ribru17/bamboo.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("bamboo").setup({})
-    require("bamboo").load()
-  end,
-}
+/home/ravindu/.config/omarchy/current/theme/neovim.lua
