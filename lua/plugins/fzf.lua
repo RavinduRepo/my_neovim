dab88f2d4735cb0b7786560cff715aac2476bc1a
RavinduRepo <ravindupathirage12345@gmail.com>
2026-05-26 17:38:24 +0530

    added leader fi to search gitignore files

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      {
+        "<leader>fi",
+        "<cmd>FzfLua files cmd='rg --files --hidden --no-ignore'<cr>",
+        desc = "Find Files (Include Ignored)",
+      },
+    },
+  },
+}
--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -6,6 +6,16 @@ return {
     init = function()
       vim.g.neominimap = {
         auto_enable = false, -- Keeps the minimap off by default
+
+        -- SCALE COMPRESSION:
+        -- How many rows of actual code a single minimap dot should span.
+        -- Default is 1. Increase this to "shrink" the text vertically
+        -- and see more of your file in the mini view.
+        y_multiplier = 4,
+
+        -- How many columns of code a dot should span.
+        -- Default is 4. Tweak this to adjust horizontal compression.
+        x_multiplier = 4,
       }
     end,
     keys = {
