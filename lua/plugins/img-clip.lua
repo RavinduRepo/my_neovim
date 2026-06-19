045da2f154587986f3cb1a09cb854c72aa4146aa
RavvinduRepo <ravindupathirage12345@.gmailcom>
2026-06-19 17:36:59 +0530

    added proper image support and compatibility to add images to notes

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -1,18 +1,31 @@
 return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
-  opts = {
-    default = {
-      -- Saves images to an 'assets' folder next to your markdown file
-      dir_path = "assets",
-      -- Keeps paths relative (e.g., ![](assets/image.png)) instead of absolute
-      use_absolute_path = false,
-      -- Set to true if you want to type a custom name for every pasted image
-      prompt_for_file_name = false,
-    },
-  },
   keys = {
-    -- Press <leader>p to paste the image
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    -- Lowercase 'p': Paste to your global Notes directory
+    {
+      "<leader>p",
+      function()
+        require("img-clip").paste_image({
+          -- vim.fn.expand translates the '~' to your actual home directory path
+          dir_path = vim.fn.expand("~/Notes/assets"),
+          use_absolute_path = true,
+        })
+      end,
+      desc = "Paste image to Global Notes",
+    },
+
+    -- Uppercase 'P': Paste to the local project directory
+    {
+      "<leader>P",
+      function()
+        require("img-clip").paste_image({
+          -- Creates an 'assets' folder relative to the current file
+          dir_path = "assets",
+          use_absolute_path = false,
+        })
+      end,
+      desc = "Paste image to Local Project",
+    },
   },
 }
